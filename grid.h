aa99f586e0f1effd95c3cf3987c552dbd5d383bd
Steven <sdlaverty123@gmail.com>
2023-04-08 13:13:20 -0400

    Updated grid utilities

--- grid.h
+++ grid.h
@@ -1,5 +1,5 @@
 #include <stdbool.h>
-#include <stddef.h>
+#include <string.h>
 
 #define NUM_ROWS 32768 // 2^15
 #define NUM_COLS 32768 // 2^15
@@ -9,17 +9,46 @@
 typedef bool **Grid;
 
 /**
+ * @brief Copy a grid row to a destination buffer.
+ *
+ * @param dest The destination buffer.
+ * @param src The source grid.
+ * @param row_idx The index of the row to copy from.
+ * @param col_start The column index to start copying from.
+ * @param col_end Copy up to, but not including this column index.
+ */
+inline void get_row(bool *dest, const Grid src, size_t row_idx, size_t col_start, size_t col_end)
+{
+    memcpy(dest, src[row_idx] + col_start, (col_end - col_start) * sizeof(bool));
+}
+
+/**
+ * @brief Copy a source buffer to a grid row.
+ *
+ * @param dest The destination grid.
+ * @param src The source buffer.
+ * @param row_idx The index of the row to copy to.
+ * @param col_start The column index to start copying to.
+ * @param col_end Copy up to, but not including this column index.
+ */
+inline void set_row(Grid dest, const bool *src, size_t row_idx, size_t col_start, size_t col_end)
+{
+    memcpy(dest[row_idx] + col_start, src, (col_end - col_start) * sizeof(bool));
+}
+
+/**
  * @brief Copy a grid column to a destination buffer.
  *
  * @param dest The destination buffer.
  * @param src The source grid.
- * @param height The height of the grid.
  * @param col_idx The index of the column to copy from.
+ * @param row_start The row index to start copying from.
+ * @param row_end Copy up to, but not including this row index.
  */
-inline void get_col(bool *dest, const Grid src, size_t height, size_t col_idx)
+inline void get_col(bool *dest, const Grid src, size_t col_idx, size_t row_start, size_t row_end)
 {
-    for (size_t i = 0; i < height; i++)
-        dest[i] = src[i][col_idx];
+    for (size_t i = row_start; i < row_end; i++)
+        dest[i - row_start] = src[i][col_idx];
 }
 
 /**
@@ -27,13 +56,14 @@ inline void get_col(bool *dest, const Grid src, size_t height, size_t col_idx)
  *
  * @param dest The destination grid.
  * @param src The source buffer.
- * @param height The height of the grid.
  * @param col_idx The index of the column to copy to.
+ * @param row_start The row index to start copying to.
+ * @param row_end Copy up to, but not including this row index.
  */
-inline void set_col(Grid dest, const bool *src, size_t height, size_t col_idx)
+inline void set_col(Grid dest, const bool *src, size_t col_idx, size_t row_start, size_t row_end)
 {
-    for (size_t i = 0; i < height; i++)
-        dest[i][col_idx] = src[i];
+    for (size_t i = row_start; i < row_end; i++)
+        dest[i][col_idx] = src[i - row_start];
 }
 
 /** A view of the global data grid. */
