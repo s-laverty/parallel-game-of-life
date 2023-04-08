6dc3190848ef088e5d4d7ab5d7ae1641223e5be4
Steven <sdlaverty123@gmail.com>
2023-04-07 21:08:38 -0400

    Added simulation file

---
+++ simulation.c
@@ -0,0 +1,14 @@
+/**
+ * @file simulation.c
+ * @author Steven Laverty (lavers@rpi.edu)
+ * @brief This file defines the procedure for parallelized game of life simulation
+ * intercommunication.
+ * @version 0.1
+ * @date 2023-04-07 
+ */
+
+#include <stdio.h>
+
+#define NUM_ROWS 32768 // 2^15
+#define NUM_COLS 32768 // 2^15
+
