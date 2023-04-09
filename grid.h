ae2d0bc68cb9ace69fc1259d2b1b58733568769b
Steven <sdlaverty123@gmail.com>
2023-04-09 14:15:33 -0400

    Convert storage to 1d array

--- grid.h
+++ grid.h
@@ -1,3 +1,11 @@
+/**
+ * @file grid.h
+ * @author Steven laverty (lavers@rpi.edu)
+ * @brief This file defines game of life data structures and utility functions.
+ * @version 0.1
+ * @date 2023-04-09
+ */
+
 #include <stdbool.h>
 #include <string.h>
 
@@ -5,8 +13,16 @@
 #define NUM_COLS 32768 // 2^15
 #define WRAP_GLOBAL_GRID true
 
-/** Game of life cell data. Indexed as [row][col]. */
-typedef bool **Grid;
+/** A grid of binary cell data. */
+typedef struct
+{
+    /** Game of life cell data. Flattened array of rows. */
+    bool *data;
+    /** Number of columns. */
+    size_t width;
+    /** Number of rows. */
+    size_t height;
+} Grid;
 
 /**
  * @brief Copy a grid row to a destination buffer.
@@ -15,11 +31,11 @@ typedef bool **Grid;
  * @param src The source grid.
  * @param row_idx The index of the row to copy from.
  * @param col_start The column index to start copying from.
- * @param width How many cells to copy.
+ * @param length How many cells to copy.
  */
-inline void get_row(bool *dest, const Grid src, size_t row_idx, size_t col_start, size_t width)
+inline void get_row(bool *dest, const Grid *src, size_t row_idx, size_t col_start, size_t length)
 {
-    memcpy(dest, src[row_idx] + col_start, width * sizeof(bool));
+    memcpy(dest, src->data + (row_idx * src->width) + col_start, length * sizeof(bool));
 }
 
 /**
@@ -29,11 +45,11 @@ inline void get_row(bool *dest, const Grid src, size_t row_idx, size_t col_start
  * @param src The source buffer.
  * @param row_idx The index of the row to copy to.
  * @param col_start The column index to start copying to.
- * @param width How many cells to copy.
+ * @param length How many cells to copy.
  */
-inline void set_row(Grid dest, const bool *src, size_t row_idx, size_t col_start, size_t width)
+inline void set_row(Grid *dest, const bool *src, size_t row_idx, size_t col_start, size_t length)
 {
-    memcpy(dest[row_idx] + col_start, src, width * sizeof(bool));
+    memcpy(dest->data + (row_idx * dest->width) + col_start, src, length * sizeof(bool));
 }
 
 /**
@@ -43,12 +59,12 @@ inline void set_row(Grid dest, const bool *src, size_t row_idx, size_t col_start
  * @param src The source grid.
  * @param col_idx The index of the column to copy from.
  * @param row_start The row index to start copying from.
- * @param height How many cells to copy.
+ * @param length How many cells to copy.
  */
-inline void get_col(bool *dest, const Grid src, size_t col_idx, size_t row_start, size_t height)
+inline void get_col(bool *dest, const Grid *src, size_t col_idx, size_t row_start, size_t length)
 {
-    for (size_t i = 0; i < height; i++)
-        dest[i] = src[row_start + i][col_idx];
+    for (size_t i = 0, j = row_start * src->width + col_idx; i < length; i++, j += src->width)
+        dest[i] = src->data[j];
 }
 
 /**
@@ -58,19 +74,19 @@ inline void get_col(bool *dest, const Grid src, size_t col_idx, size_t row_start
  * @param src The source buffer.
  * @param col_idx The index of the column to copy to.
  * @param row_start The row index to start copying to.
- * @param height How many cells to copy.
+ * @param length How many cells to copy.
  */
-inline void set_col(Grid dest, const bool *src, size_t col_idx, size_t row_start, size_t height)
+inline void set_col(Grid *dest, const bool *src, size_t col_idx, size_t row_start, size_t length)
 {
-    for (size_t i = 0; i < height; i++)
-        dest[row_start + i][col_idx] = src[i];
+    for (size_t i = 0, j = row_start * dest->width + col_idx; i < length; i++, j += dest->width)
+        dest->data[j] = src[i];
 }
 
 /** A rectangular view of the global data grid with 1-cell padding. */
 typedef struct
 {
     /** Data in the grid view. */
-    Grid grid;
+    Grid *grid;
     /** Starting global row index of the grid view. */
     size_t row_start;
     /** Number of rows in the grid view (NOT including 1-cell padding). */
