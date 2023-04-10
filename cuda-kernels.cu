fd0ed256685bfcb4efb7499a36b89a57abba3e6f
aHarry01 <67612176+aHarry01@users.noreply.github.com>
2023-04-10 14:09:18 -0400

    Fixed bug when width*height not divisible by threads_per_block

--- cuda-kernels.cu
+++ cuda-kernels.cu
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 #include "clockcycle.h"
 
 #define clock_frequency 512000000
-#define threads_per_block 10 
+#define threads_per_block 12
 #define WIDTH 10
 #define HEIGHT 10
 
@@ -26,7 +27,6 @@ bool board_template[WIDTH][HEIGHT] = {
 };
 
 
-//TODO: what if width*height / threads_per_block is not an integer
 //TODO: pipelined kernel that will pipeline time steps
 //		each thread waits until neighbors are at same generation as itself before continuing, work on calculating multiple generations at once
 
@@ -40,30 +40,32 @@ bool board_template[WIDTH][HEIGHT] = {
  * @param height Grid height
  */
 __global__ void compute_timestep(bool* grid, bool* next_grid, int width, int height){
-  // Each thread the next state for 1 cell
-  unsigned int i = blockIdx.x*blockDim.x + threadIdx.x; 
-  unsigned int col = i % width;
-  unsigned int row = i / height;
-  // calculate wrapped-around values
-  unsigned int col_plus1 = (col < width-1) ? col+1 : 0;
-  unsigned int col_minus1 = (col > 0) ? col-1 : width-1;
-  unsigned int row_plus1 = (row < height-1) ? row+1 : 0;
-  unsigned int row_minus1 = (row > 0) ? row-1 : height-1;
+	// Each thread the next state for 1 cell
+	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x; 
+	if (i < width*height){
+		unsigned int col = i % width;
+		unsigned int row = i / height;
+		// calculate wrapped-around values
+		unsigned int col_plus1 = (col < width-1) ? col+1 : 0;
+		unsigned int col_minus1 = (col > 0) ? col-1 : width-1;
+		unsigned int row_plus1 = (row < height-1) ? row+1 : 0;
+		unsigned int row_minus1 = (row > 0) ? row-1 : height-1;
 
-  // count the num of alive cells surrounding 
-  unsigned int surrounding_population = 
-  	grid[width*row_minus1 + col_minus1] + 
-  	grid[width*row_minus1 + col] + 
-  	grid[width*row_minus1 + col_plus1] + 
-  	grid[width*row + col_minus1] + 
-  	grid[width*row + col_plus1] + 
-   	grid[width*row_plus1 + col_minus1] + 
-  	grid[width*row_plus1 + col] + 
-  	grid[width*row_plus1 + col_plus1];
-	
-  // Set next cell state
-  bool next_cell_state = (surrounding_population == 3 || (grid[width*row + col] && surrounding_population==2));
-  next_grid[width*row + col] = next_cell_state;
+		// count the num of alive cells surrounding 
+		unsigned int surrounding_population = 
+			grid[width*row_minus1 + col_minus1] + 
+		  	grid[width*row_minus1 + col] + 
+		  	grid[width*row_minus1 + col_plus1] + 
+		  	grid[width*row + col_minus1] + 
+		  	grid[width*row + col_plus1] + 
+		   	grid[width*row_plus1 + col_minus1] + 
+		  	grid[width*row_plus1 + col] + 
+		  	grid[width*row_plus1 + col_plus1];
+			
+		// Set next cell state
+		bool next_cell_state = (surrounding_population == 3 || (grid[width*row + col] && surrounding_population==2));
+		next_grid[width*row + col] = next_cell_state;	
+	  }
 }
 
 // temporary main for testing CUDA kernel
@@ -92,14 +94,9 @@ int main(int argc, char *argv[]){
 	start_cycles = clock_now();
 
 	// call kernel
-	unsigned int grid_size = (HEIGHT*WIDTH) / threads_per_block;
+	unsigned int grid_size = ceil((HEIGHT*WIDTH) / (float)threads_per_block);
 	compute_timestep<<<grid_size, threads_per_block>>>(board, output_board, WIDTH, HEIGHT);
 	cudaDeviceSynchronize();
-	// check for errors
-	cudaError_t error = cudaGetLastError();
-	if (error != cudaSuccess) {
-	  fprintf(stderr, "ERROR: %s \n", cudaGetErrorString(error));
-	}
 
 	end_cycles = clock_now();
 	printf("Finished in %lf\n",  ((double)(end_cycles-start_cycles))/clock_frequency);
