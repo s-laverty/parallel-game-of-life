f5513764739215816ee5b2c735ec95e7a208e216
aHarry01 <67612176+aHarry01@users.noreply.github.com>
2023-04-13 19:36:33 -0400

    Fixed pipelined implementation to work when HEIGHT not divisible by ROWS_PER_GPU

--- pipelined.c
+++ pipelined.c
@@ -11,19 +11,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <mpi.h>
+#include <math.h>
 
 #include "clockcycle.h"
 
-//TODO: what if HEIGHT is not divisible by ROWS_PER_GPU?
 //TODO: what if TIMESTEPS != # MPI ranks? should wrap around...
 //TODO: test wrapping with live cells near edges
 //TODO: test performance vs only GPU calls
 
 // these have to match the WIDTH & HEIGHT in cuda-kernels.cu
 #define WIDTH 20
-#define HEIGHT 20
+#define HEIGHT 11
 #define TIMESTEPS 4 // number of generations to calculate
-#define ROWS_PER_GPU 5 //number of rows computed by each GPU every time step
+#define ROWS_PER_GPU 4 //number of rows computed by each GPU every time step
 #define clock_frequency 512000000
 
 void cuda_init(bool** grid, bool** next_grid, int my_rank);
@@ -33,6 +33,7 @@ void free_cudamemory(bool* grid, bool* next_grid);
 
 // this board template is just for convenient storage of 
 // initial test board
+
 bool grid_template[10][10] = {
 	{false, false, false, false, false, false, false, false, false, false},
 	{false, false, false, false, false, false, false, false, false, false},
@@ -45,6 +46,7 @@ bool grid_template[10][10] = {
 	{false, false, false, false, false, false, false, true, false, true},
 	{false, false, false, false, false, false, false, false, true, true},
 };
+
 
 int main(int argc, char** argv){
 
@@ -85,24 +87,35 @@ int main(int argc, char** argv){
   	unsigned long long end_cycles=clock_now();   // dummy clock reads to init
 	start_cycles = clock_now();
 
-	// Synchronize between each time step
 	int my_t = 0; // time step for this rank
 	int row_start = 0;
+	int row_end = 0;
+	int prev_rank_offset = my_rank-1;
+	if (prev_rank_offset < 0) prev_rank_offset = num_ranks; //wrap around
+	int offset = my_rank; //row offset (ex. when offset = 3, will calculate rows 3-8 instead of 0-5)
 	MPI_Status* status;
 	MPI_Request* req;
-	for(int t = 0; t < (TIMESTEPS-1)*2 + HEIGHT/ROWS_PER_GPU; t++){
+	unsigned int iters = ceil((double)HEIGHT/ROWS_PER_GPU); //# iterations for this rank for a single timestep
+	for(int t = 0; t < (TIMESTEPS-1)*2 + iters; t++){
 		// Calculate one section of this rank's generation and send to next rank
 		// so that the next rank can get started with the next generation
-		if(t >= 2*my_rank && my_t < HEIGHT/ROWS_PER_GPU){
+		if(t >= 2*my_rank && my_t < iters){
 			row_start = my_t*ROWS_PER_GPU+my_rank;
-			printf("Rank %d processes rows %d to %d at t=%d\n", my_rank, row_start, row_start+ROWS_PER_GPU-1, t);
-			run_kernel_section(grid, next_grid, WIDTH, HEIGHT, row_start, row_start+ROWS_PER_GPU-1);
+			row_end = row_start+ROWS_PER_GPU-1;
+			if(row_end > HEIGHT+offset-1) row_end = HEIGHT+offset-1;
+			printf("Rank %d processes rows %d to %d at t=%d\n", my_rank, row_start, row_end, t);
+			run_kernel_section(grid, next_grid, WIDTH, HEIGHT, row_start, row_end);
 			//transfer data to the next rank
 			if (my_rank+1 < num_ranks){
-				printf("Rank %d sends data to %d starting at %d at t=%d\n", my_rank, my_rank+1, row_start, t);
-				if (row_start+ROWS_PER_GPU-1 >= HEIGHT){ //rows wrap around so must send in 2 chunks
+				printf("Rank %d sends rows %d to %d to rank %d at t=%d\n",  my_rank, row_start, row_end, my_rank+1, t);
+				if (row_end >= HEIGHT && row_start >= HEIGHT){ //entirely wrap around
+					row_start = row_start - HEIGHT;
+					row_end = row_end - HEIGHT;
+					MPI_Send(next_grid+WIDTH*row_start, WIDTH*ROWS_PER_GPU, MPI_C_BOOL, my_rank+1, 0, MPI_COMM_WORLD);
+				}
+				if (row_end >= HEIGHT){ //rows wrap around so must send in 2 chunks
 					MPI_Send(next_grid+WIDTH*row_start, WIDTH*(HEIGHT-row_start), MPI_C_BOOL, my_rank+1, 0, MPI_COMM_WORLD);
-					MPI_Send(next_grid, WIDTH*(ROWS_PER_GPU-(HEIGHT-row_start)), MPI_C_BOOL, my_rank+1, 0, MPI_COMM_WORLD);
+					MPI_Send(next_grid, WIDTH*((row_end-row_start)-(HEIGHT-row_start)), MPI_C_BOOL, my_rank+1, 0, MPI_COMM_WORLD);
 				}
 				else{ // don't have to wrap around
 					MPI_Send(next_grid+WIDTH*row_start, WIDTH*ROWS_PER_GPU, MPI_C_BOOL, my_rank+1, 0, MPI_COMM_WORLD);
@@ -112,16 +125,23 @@ int main(int argc, char** argv){
 		}
 
 		// Receive data from previous rank which is computing the previous generation
-		if(my_rank > 0 && t >= 2*(my_rank-1) && my_t < HEIGHT/ROWS_PER_GPU-1){
+		if(my_rank > 0 && t >= 2*(my_rank-1) && my_t < iters-1){
 			int row_start = ROWS_PER_GPU*(t- 2*(my_rank-1)) + my_rank-1;
-			if (row_start+ROWS_PER_GPU-1 >= HEIGHT){ //rows wrap around so it was sent in 2 chunks
-				MPI_Recv(next_grid+WIDTH*row_start, WIDTH*(HEIGHT-row_start), MPI_C_BOOL, my_rank-1, 0, MPI_COMM_WORLD, status);
-				MPI_Recv(next_grid, WIDTH*(ROWS_PER_GPU-(HEIGHT-row_start)), MPI_C_BOOL, my_rank-1, 0, MPI_COMM_WORLD, status);
+			int row_end = row_start+ROWS_PER_GPU-1;
+			if(row_end > HEIGHT+prev_rank_offset-1) row_end = HEIGHT+prev_rank_offset-1;
+			if (row_end >= HEIGHT && row_start >= HEIGHT){ //entirely wrap around
+				row_start = row_start - HEIGHT;
+				row_end = row_end - HEIGHT;
+				MPI_Recv(grid+WIDTH*row_start, WIDTH*ROWS_PER_GPU, MPI_C_BOOL, my_rank-1, 0, MPI_COMM_WORLD, status);
+			}
+			else if (row_end >= HEIGHT){ //rows wrap around so it was sent in 2 chunks
+				MPI_Recv(grid+WIDTH*row_start, WIDTH*(HEIGHT-row_start), MPI_C_BOOL, my_rank-1, 0, MPI_COMM_WORLD, status);
+				MPI_Recv(grid, WIDTH*((row_end-row_start)-(HEIGHT-row_start)), MPI_C_BOOL, my_rank-1, 0, MPI_COMM_WORLD, status);
 			}
 			else{ // don't have to wrap around
 				MPI_Recv(grid+WIDTH*row_start, WIDTH*ROWS_PER_GPU, MPI_C_BOOL, my_rank-1, 0, MPI_COMM_WORLD, status);
 			}
-			printf("Rank %d receives data from %d starting at %d at t=%d\n", my_rank, my_rank - 1, row_start, t);
+			printf("Rank %d receives rows %d to %d from %d at t=%d\n", my_rank, row_start, row_end, my_rank - 1, t);
 		}
 		//MPI_Barrier(MPI_COMM_WORLD);
 	}
@@ -130,7 +150,7 @@ int main(int argc, char** argv){
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	// Print output
-	if (my_rank == 1){
+	if (my_rank == 3){
 		printf("\nRank %d output (%d generations have passed)\n", my_rank, my_rank+1);
 		for(int r = 0; r < HEIGHT; r++){
 			for(int c = 0; c < WIDTH; c++){
