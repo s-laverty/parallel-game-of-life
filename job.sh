8502362d59e46cc60f4b990c5360e4caddb7673d
Steven <sdlaverty123@gmail.com>
2023-04-21 09:52:53 -0400

    bash script fix

--- job.sh
+++ job.sh
@@ -26,7 +26,7 @@ taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s m brick 100
 taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s l brick 10
 
 # xl tests
-if [ $xl = true ]; then
+if [[ $xl = true ]]; then
 taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s xl brick 1
 taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s xl striped 1
 fi
