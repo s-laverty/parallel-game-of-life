f63997a2ec040f0c7bf1414ee1dc6c93574303c4
Steven <sdlaverty123@gmail.com>
2023-04-21 09:45:19 -0400

    updated rpi scripts

--- job-rpi.sh
+++ job-rpi.sh
@@ -18,13 +18,13 @@ module load spectrum-mpi
 # Striped tests
 taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s s striped 1000
 taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s m striped 100
-if [ $l = true ]; then
-taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s l striped 10
-fi
 
 # Brick tests
 taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s s brick 1000
 taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s m brick 100
+
+# Large tests
 if [ $l = true ]; then
 taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s l brick 10
+taskset -c 0-159:4 mpirun -N 6 simulation -i acorn -s l striped 10
 fi
