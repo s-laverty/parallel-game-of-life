044af2eceef7033826eebd11238ecee11fb481b3
Steven <sdlaverty123@gmail.com>
2023-04-21 09:47:30 -0400

    updated batch script base on vram

--- batch.sh
+++ batch.sh
@@ -4,7 +4,7 @@ nodes=(1 2 4 8 16)
 
 for i in {0..4}
 do
-    if (( nodes[i] < 8 )); then
+    if (( nodes[i] < 16 )); then
     sbatch -N ${nodes[i]} -o ${nodes[i]}_nodes.out job.sh
     else
     sbatch -N ${nodes[i]} -o ${nodes[i]}_nodes.out job.sh -l
